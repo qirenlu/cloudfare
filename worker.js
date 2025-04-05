dd7be7511e1c775db472a64bb7f7ad7873aba928
qirenlu <152314778+qirenlu@users.noreply.github.com>
2025-04-06 02:56:52 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,6 +1,6 @@
 export default {
   async fetch(request, env, ctx) {
-    const githubUrl = 'https://cdn.jsdelivr.net/gh/你的用户名/你的仓库名@main/latest.txt?t=' + Date.now();
+    const githubUrl = 'https://www.notion.so/https-times1740911335-subtangniu-top-9606-v2b-catnet-api-v1-client-subscribe-token-10ac210b8932bd1-1cc019f2b448806a957de6b69db2fae8?pvs=4' + Date.now();
 
     const res = await fetch(githubUrl);
     const subUrl = (await res.text()).trim();
